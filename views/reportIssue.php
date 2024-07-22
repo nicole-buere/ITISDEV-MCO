d721a35d7ff592df951b0f8a80d9890e169b8c7b
Jsf <josef_michael_tan@dlsu.edu.ph>
2024-07-22 13:12:33 +0800

    update to reportIssue

--- views/reportIssue.php
+++ views/reportIssue.php
@@ -3,8 +3,19 @@
         <title>Report an issue</title>
     </head>
     <body>
-    <?php
-
-    ?>  
+        <div class="formContainer">
+            <h1>Report an Issue</h1>
+            <form method="POST">
+                <label for="reportType">Type:</label>
+                <select name="reportType">
+                    <option value="sample1">Sample1</option>
+                    <option value="sample2">Sample2</option>
+                    <option value="sample3">Sample3</option>
+                    <option value="sample4">Sample4</option>
+                    <option value="sample5">Sample5</option>
+                </select>
+                <input type="submit" value="Submit Report"></input>
+            </form>
+        </div>
     </body>
 </html>
