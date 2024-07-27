7ba077891e8eafea4c38665b827a063962e0e80e
Nicole Buere <140893902+nicole-buere@users.noreply.github.com>
2024-07-27 21:28:01 +0800

    sql for db and db connection added

---
+++ includes/db.php
@@ -0,0 +1,16 @@
+<?php
+// includes/db.php
+
+$servername = "localhost";
+$username = "root"; 
+$password = ""; // no password by default
+$dbname = "dbcityease"; // The database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
