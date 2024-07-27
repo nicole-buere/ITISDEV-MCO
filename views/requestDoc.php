6d192fd9afb4d7ceef5dbefdce5fd78fe54c64ea
larawr <103736158+larawr@users.noreply.github.com>
2024-07-27 19:49:50 +0800

    made payment.php and added provinces in requestDoc

--- views/requestDoc.php
+++ views/requestDoc.php
@@ -50,12 +50,24 @@
                             <div class="inputDiv">
                                 <label for="region" class="formFieldLabel">Region:</label>
                                 <select required class="region" name="region">
-                                    <option hidden disabled selected value class="selectOption">Select an option</option>
-                                    <option class="selectOption" value="sample1">Sample1</option>
-                                    <option class="selectOption" value="sample2">Sample2</option>
-                                    <option class="selectOption" value="sample3">Sample3</option>
-                                    <option class="selectOption" value="sample4">Sample4</option>
-                                    <option class="selectOption" value="sample5">Sample5</option>
+                                    <option hidden disabled selected value class="selectOption">Select a region</option>
+                                    <option class="selectOption" value="ncr">National Capital Region (NCR)</option>
+                                    <option class="selectOption" value="car">Cordillera Administrative Region (CAR)</option>
+                                    <option class="selectOption" value="region1">Ilocos Region (Region I)</option>
+                                    <option class="selectOption" value="region2">Cagayan Valley (Region II)</option>
+                                    <option class="selectOption" value="region3">Central Luzon (Region III)</option>
+                                    <option class="selectOption" value="region4a">Calabarzon (Region IV-A)</option>
+                                    <option class="selectOption" value="region4b">Mimaropa (Region IV-B)</option>
+                                    <option class="selectOption" value="region5">Bicol Region (Region V)</option>
+                                    <option class="selectOption" value="region6">Western Visayas (Region VI)</option>
+                                    <option class="selectOption" value="region7">Central Visayas (Region VII)</option>
+                                    <option class="selectOption" value="region8">Eastern Visayas (Region VIII)</option>
+                                    <option class="selectOption" value="region9">Zamboanga Peninsula (Region IX)</option>
+                                    <option class="selectOption" value="region10">Northern Mindanao (Region X)</option>
+                                    <option class="selectOption" value="region11">Davao Region (Region XI)</option>
+                                    <option class="selectOption" value="region12">Soccsksargen (Region XII)</option>
+                                    <option class="selectOption" value="caraga">Caraga (Region XIII)</option>
+                                    <option class="selectOption" value="bar">Bangsamoro Autonomous Region in Muslim Mindanao (BARMM)</option>
                                 </select>
                             </div>
                         </td>
