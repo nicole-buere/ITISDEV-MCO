cb72b83c7c1860995621e309f3b03be1a990bc29
Nicole Buere <140893902+nicole-buere@users.noreply.github.com>
2024-07-28 16:45:24 +0800

    about page added, homepage content modified

---
+++ views/aboutus.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CityEase About Us</title>
+    <link rel="stylesheet" href="../css/aboutus.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/footer.css">
+</head>
+<body>
+    <header>
+        <div class="logo-container">
+            <img src="../assets/logo-noname.png" alt="Logo" class="header-logo">
+            <h2>CityEase</h2>
+        </div>
+        <nav>
+            <a class="first-button" href="../views/homepage.php">Home</a>
+            <a class="first-button" href="../views/aboutus.php">About Us</a>
+            <div class="dropdown">
+                <button class="dropbtn">Services</button>
+                <div class="dropdown-content">
+                    <a href="../views/reportIssue.php">Report an Issue</a>
+                    <a href="../views/requestDoc.php">Request Document</a>
+                </div>
+            </div>
+            <a class="first-button" href="../views/Transactions.php">Transactions</a>
+        </nav>
+        <div class="dropdown profile-dropdown">
+            <img src="../assets/profile-user.png" alt="profile" class="profile dropbtn">
+            <div class="dropdown-content">
+                <a href="../views/profile.php"><i class="fas fa-user"></i> View Profile</a>
+                <a href="../views/settings.php"><i class="fas fa-cog"></i> Profile Settings</a>
+                <a href="../views/index.php"><i class="fas fa-sign-out-alt"></i> Logout</a>
+            </div>
+        </div>
+    </header>
+
+    <div class="main-content">
+        <div class="intro">
+            <img src="../assets/smart-city.jpg" alt="Smart City" class="header-logo">
+            <div>
+                <h1>Making Smart Cities Happen</h1>
+                <p>CityEase is a comprehensive software solution designed to empower Local Government Units (LGUs) in the Philippines to digitally transform and advance cities, municipalities, and barangays into self-reliant communities, known as Smart Cities.</p>
+                <p>Developed with the goal of providing more digital accessibility to key city services and infrastructures such as transportation, electricity, water, healthcare, and more, CityEase enables Smart Cities like yours to actively engage with its citizens and their well-being.</p>
+            </div>
+        </div>
+
+        <div class="creators">
+            <h2>Meet Our Team</h2>
+            <p>This is created by Group 5 of the ITISDEV Course, of De La Salle University Manila</p>
+            <div class="team-members">
+                <div class="member">
+                    <h2>Nicole Buere</h2>
+                </div>
+                <div class="member">
+                    <h2>Luisa Jacob</h2>
+                </div>
+                <div class="member">
+                    <h2>Lara Pegalan</h2>
+                </div>
+                <div class="member">
+                    <h2>Nadeen Ramos</h2>
+                </div>
+                <div class="member">
+                    <h2>Josef Tan</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer">
+        <div class="left-text">
+            <p>&copy; CityEase 2024</p>
+        </div>
+        <p class="centered-footer-text">YOUR ONE-STOP PLATFORM FOR SEAMLESS COMMUNITY MANAGEMENT</p>
+    </footer>
+</body>
+</html>
--- views/homepage.php
+++ views/homepage.php
@@ -1,58 +1,99 @@
-<!--homepage -->
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CityEase Homepage</title>
     <link rel="stylesheet" href="../css/homepage.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/footer.css">
 </head>
 <body>
     <header>
-	<div class="logo-container">
-        <img src="../assets/logo-noname.png" alt="Logo" class="header-logo">
-        <h2>CityEase</h2>
-    </div>
-	<a class="first-button" href="homepage.html">Home</a>
-	<a class="first-button" href="reportIssue.html">Report an Issue</a>
-	<a class="first-button" href="requestDoc.html">Request Document</a>
- 	<button class="second-button" href="index.php">Log In</button>
-    <button class="second-button" href="sign-up.php">Sign Up</button>
+        <div class="logo-container">
+            <img src="../assets/logo-noname.png" alt="Logo" class="header-logo">
+            <h2>CityEase</h2>
+        </div>
+        <nav>
+            <a class="first-button" href="../views/homepage.php">Home</a>
+            <a class="first-button" href="../views/aboutus.php">About Us</a>
+            <div class="dropdown">
+                <button class="dropbtn">Services</button>
+                <div class="dropdown-content">
+                    <a href="../views/reportIssue.php">Report an Issue</a>
+                    <a href="../views/requestDoc.php">Request Document</a>
+                </div>
+            </div>
+            <a class="first-button" href="../views/Transactions.php">Transactions</a>
+        </nav>
+        <div class="dropdown profile-dropdown">
+            <img src="../assets/profile-user.png" alt="profile" class="profile dropbtn">
+            <div class="dropdown-content">
+                <a href="../views/profile.php"><i class="fas fa-user"></i> View Profile</a>
+                <a href="../views/settings.php"><i class="fas fa-cog"></i> Profile Settings</a>
+                <a href="../views/index.php"><i class="fas fa-sign-out-alt"></i> Logout</a>
+            </div>
+        </div>
     </header>
 
-    <main>
-        <div class="main-background">
-
-        </div>
+    <div class="main-content">
         <div class="top-section">
-            <img src="../assets/logo-noname.png" alt="Logo" class="main-logo">
-            <h3>Welcome to CityEase!</h3>
-            <p>YOUR ONE-STOP PLATFORM FOR SEAMLESS COMMUNITY MANAGEMENT.</p>
-            <p>At CityEase, we are dedicated to enchancing the efficiency and accessibility of local government services for</p>
-            <p>barangays across our city. Our mission is to streamline the way citizens interact with their local government,</p>
-            <p>making it easier to report issues, manage documents, and stay informed about community updates.</p>
+            <div class="text-box">
+                <h3>Welcome to CityEase!</h3>
+                <h4>YOUR ONE-STOP PLATFORM FOR SEAMLESS COMMUNITY MANAGEMENT.</h4>
+                <br>
+                <p>At CityEase, we are dedicated to enhancing the efficiency and accessibility of local government services for</p>
+                <p>barangays across our city. Our mission is to streamline the way citizens interact with their local government,</p>
+                <p>making it easier to report issues, manage documents, and stay informed about community updates.</p>
+            </div>
         </div>
-        <div class="bottom-section">
-        <div class="left-section">
-            <h1>YOUR GATEWAY</h1>
-            <h1>TO LOCAL SERVICES</h1>
-            <button class="main-button" href="sign-up.php">Sign Up</button>
-        </div>
-        <div class="right-section">
-            <p>Welcome to CityEase, where community management meets simplicity. Our platform offers a user-</p>
-            <p>friendly interface for reporting issues, managing documents, and accesing local services with ease.</p>
-            <p>Dedicated to enhancing transparency and citizen engagement, CityEase is your partner in building a</p>
-            <p>connected and responsive community.</p>
-            <p>Join us in shaping a better future for our city through efficient, accessible, and innovative local</p>
-            <p>governance solutions.</p>
-        </div>
-        </div>
-    </main>
 
-    <footer>
-	<div class="left-text">
-	     <p>&copy; CityEase 2024</p>
-	</div>
+        <div class="bottom-section">
+            <div class="service">
+                <h2>YOUR GATEWAY TO LOCAL SERVICES</h2>
+                <div class="row-box">
+                    <!-- for request documents -->
+                    <div class="info-box">
+                        <div class="img-box">
+                            <img src="../assets/request.png" alt="Request Documents" class="profile">
+                        </div>
+                        <div class="description">
+                            <h4>Request Documents</h4>
+                            <p>Skip the trip to your barangay office and the long queues.</p>
+                            <p>Simply request all your essential documents with a few clicks right here!</p>
+                        </div>
+                    </div>
+                    <!-- for Report an Issue -->
+                    <div class="info-box">
+                        <div class="img-box">
+                            <img src="../assets/report.png" alt="Report an Issue" class="profile">
+                        </div>
+                        <div class="description">
+                            <h4>Report an Issue</h4>
+                            <p>No need to visit the barangay office to voice your concerns.</p>
+                            <p>Just click, report, and get a quick response to any issue in your community!</p>
+                        </div>
+                    </div>
+                </div>        
+            </div>
+
+            <div class="about-cityease">
+                <div class="text-content">
+                    <h2>CityEase</h2>
+                    <hr>
+                    <p>CityEase is your comprehensive platform for effortless community management. Our user-friendly interface empowers you to report issues, manage documents, and access local services with ease.</p>
+                    <p>Designed to enhance transparency and citizen engagement, CityEase connects you with your local government, ensuring your voice is heard and your needs are promptly addressed.</p>
+                    <p>Embrace efficient, accessible, and innovative governance solutions with CityEase, and help shape a responsive and connected community.</p>
+                </div>
+                <img src="../assets/logowtext.png" alt="CityEase Logo">
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer">
+        <div class="left-text">
+            <p>&copy; CityEase 2024</p>
+        </div>
         <p class="centered-footer-text">YOUR ONE-STOP PLATFORM FOR SEAMLESS COMMUNITY MANAGEMENT</p>
     </footer>
 </body>
